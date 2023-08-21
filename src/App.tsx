9420750f8d845817814240e1772ab5e09ba2d9e8
DmitriyBarhonov <explay167@gmail.com>
2023-08-21 19:31:04 +0300

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from './components/ui/button'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <Button as="a" href={'dwwddw'}>
+      2123
+    </Button>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+// eslint-disable-next-line import/order, import/default
 import React from 'react'
+// eslint-disable-next-line import/order, import/default
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './styles/index.scss'
 import { App } from './App'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 
+// eslint-disable-next-line import/no-named-as-default-member
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
