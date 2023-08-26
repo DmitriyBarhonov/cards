6114e4bce4431b9c2151db2442690546ef4611d0
victoriapavlova <victoria.pavlova7@gmail.com>
2023-08-26 15:25:00 +0300

    feature/card - added card component with scss, stories, fixed index

---
+++ src/components/ui/card/card.tsx
@@ -0,0 +1,10 @@
+import { ComponentPropsWithoutRef, forwardRef } from 'react'
+
+import s from './card.module.scss'
+
+export type CardProps = {} & ComponentPropsWithoutRef<'div'>
+export const Card = forwardRef<HTMLDivElement, CardProps>(({ className, ...restProps }, ref) => {
+  return <div ref={ref} className={s.card || className} {...restProps}></div>
+})
+//forwardRef оборачиваем фунцию для того чтобы испольтзовать реф в детях
+//чтобы у компоненты были все типы обычного дива прикрепляем HTMLDivElement
