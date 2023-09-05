ff5e1a027ad86dae4b70dfd8306c6de7cad021ba
krlkonstantine <krlkonstantine@gmail.com>
2023-09-05 10:07:04 +0300

    tag types added

--- src/services/decks/decks.ts
+++ src/services/decks/decks.ts
@@ -5,6 +5,9 @@ const deskApi = baseApi.injectEndpoints({
     return {
       getDecks: builder.query<any, void>({
         query: () => `v1/decks`,
+        //когда выполнится запрос и даные придут
+        //они будут храниться в кэше под тегом Decks
+        providesTags: ['Decks'],
       }),
       createDeck: builder.mutation<any, { name: string }>({
         query: ({ name }) => ({
@@ -12,6 +15,9 @@ const deskApi = baseApi.injectEndpoints({
           method: 'POST',
           body: { name },
         }),
+        //когда выполнится запрос и даные придут
+        //сд. прошлые данные с этим тегом невалидными
+        invalidatesTags: ['Decks'],
       }),
     }
   },
