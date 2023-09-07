f4385de67ef76a6670b1651512406fd95f5bf9a1
krlkonstantine <krlkonstantine@gmail.com>
2023-09-07 21:35:55 +0300

    controlled input implemented to sign in

--- src/components/auth/sign-in/sign-in.tsx
+++ src/components/auth/sign-in/sign-in.tsx
@@ -1,12 +1,12 @@
 import { clsx } from 'clsx'
-import { useForm } from 'react-hook-form'
+import { FormProvider, useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 
 import s from './sign-in.module.scss'
 
 import { Button, Card, Typography } from '@/components/ui'
 import { ControlledCheckbox } from '@/components/ui/controlled'
-import { CardsInput } from '@/components/ui/input'
+import { ControlledInput } from '@/components/ui/controlled-input'
 
 export type SignInProps = {
   value?: string
@@ -37,6 +37,7 @@ export const SignIn = () => {
     question: clsx(s.bottomText),
     signUp: clsx(s.signUpLink),
   }
+  const methods = useForm()
 
   // register который мы передаем в формы
   // name: "email"
@@ -47,49 +48,51 @@ export const SignIn = () => {
   return (
     <div className={classNames.formContainer}>
       <Card className={classNames.card}>
-        <form className={classNames.form} onSubmit={handleSubmit(onSubmit)}>
-          <Typography className={classNames.mainTitle} variant={'large'}>
-            {' '}
-            Sign In
-          </Typography>
-          <CardsInput
-            className={classNames.input}
-            variant={'standard'}
-            {...register('email')}
-            label={'Email'}
-          />
-          <CardsInput
-            className={classNames.input}
-            variant={'password'}
-            {...register('password')}
-            label={'Password'}
-          />
-          <ControlledCheckbox
-            // name передаем такой же как и в типизации
-            name="rememberMe"
-            className={classNames.rememberMe}
-            label={'Remember me'}
-            // Передаем control
-            control={control}
-          />
+        <FormProvider {...methods}>
+          <form className={classNames.form} onSubmit={handleSubmit(onSubmit)}>
+            <Typography className={classNames.mainTitle} variant={'large'}>
+              {' '}
+              Sign In
+            </Typography>
+            <ControlledInput
+              className={classNames.input}
+              variant={'standard'}
+              {...register('email')}
+              label={'Email'}
+            />
+            <ControlledInput
+              className={classNames.input}
+              variant={'password'}
+              {...register('password')}
+              label={'Password'}
+            />
+            <ControlledCheckbox
+              // name передаем такой же как и в типизации
+              name="rememberMe"
+              className={classNames.rememberMe}
+              label={'Remember me'}
+              // Передаем control
+              control={control}
+            />
 
-          <Typography as={'a'} href={''} className={classNames.forgotPass} variant={'link1'}>
+            <Typography as={'a'} href={''} className={classNames.forgotPass} variant={'link1'}>
+              {' '}
+              <Link to="/forgot-password">Forgot Password?</Link>
+            </Typography>
+            <Button fullWidth={false} className={classNames.submit} type="submit">
+              Sign in
+            </Button>
+          </form>
+          <Typography className={classNames.question} variant={'body2'}>
             {' '}
-            <Link to="/forgot-password">Forgot Password?</Link>
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            Don't have an account?
           </Typography>
-          <Button fullWidth={false} className={classNames.submit} type="submit">
-            Sign in
-          </Button>
-        </form>
-        <Typography className={classNames.question} variant={'body2'}>
-          {' '}
-          {/* eslint-disable-next-line react/no-unescaped-entities */}
-          Don't have an account?
-        </Typography>
-        <Typography as={'a'} href={''} className={classNames.signUp} variant={'h3'}>
-          {' '}
-          <Link to="/sign-up">Sign Up</Link>
-        </Typography>
+          <Typography as={'a'} href={''} className={classNames.signUp} variant={'h3'}>
+            {' '}
+            <Link to="/sign-up">Sign Up</Link>
+          </Typography>
+        </FormProvider>
       </Card>
     </div>
   )
