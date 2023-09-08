85f64739035334458ba2efea7051e0613c7969ff
krlkonstantine <krlkonstantine@gmail.com>
2023-09-08 10:53:41 +0300

    added logOut icon. implemented edit mode for personal info. Styles for edit mode applied properly

--- src/components/auth/personal-info/personal-info.tsx
+++ src/components/auth/personal-info/personal-info.tsx
@@ -1,61 +1,82 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 import { clsx } from 'clsx'
 import { Link } from 'react-router-dom'
 
 import s from './personal-info.module.scss'
 
-import { Avatar, Button, Card, Typography } from '@/components/ui'
+import { EdittextIcon } from '@/assets/icons/editText.tsx'
+import { LogOut } from '@/assets/icons/logOut.tsx'
+import { Avatar, Button, Card, CardsInput, Typography } from '@/components/ui'
 
 export type PersonalInfoProps = {
   name?: string
   avatar?: string
-  onInputValueChange?: (value: string) => void
+  email?: string
 }
 
-export const PersonalInfo: FC<PersonalInfoProps> = ({ name, avatar, ...restProps }) => {
+export const PersonalInfo: FC<PersonalInfoProps> = ({ name, avatar, email }) => {
+  const [editMode, setEditMode] = useState(false)
+
   const classNames = {
     formContainer: clsx(s.formContainer),
     card: clsx(s.card),
     form: clsx(s.formBody),
     mainTitle: clsx(s.formTitle),
+    avatar: clsx(s.avatarContainer),
     input: clsx(s.formInput),
     info: clsx(s.secondaryText),
     submit: clsx(s.submitButton),
     question: clsx(s.bottomText),
     signIn: clsx(s.signLink),
   }
+  const emailForPersonalInfo = email ? email : 'ihateincubatorexams@gmail.com'
   const nameForPersonalInfo = name ? name : 'Kilobucks Lover'
   const imgForPersonalInfo = avatar
     ? avatar
     : 'https://kartinkof.club/uploads/posts/2022-03/1648314602_1-kartinkof-club-p-negr-dumaet-mem-1.jpg'
 
+  const onEditTextClickHandler = () => {
+    setEditMode(true)
+  }
+
   return (
     <div className={classNames.formContainer}>
       <Card className={classNames.card}>
-        <form className={classNames.form}>
+        <div className={classNames.form}>
           <Typography className={classNames.mainTitle} variant={'large'}>
-            {' '}
             Personal Information
           </Typography>
-          <Avatar name={nameForPersonalInfo} avatar={imgForPersonalInfo} />
+          <div>
+            <Avatar
+              className={classNames.avatar}
+              name={nameForPersonalInfo}
+              avatar={imgForPersonalInfo}
+            />
+          </div>
 
-          <Typography className={classNames.info} variant={'body2'}>
-            {' '}
-            Enter your email address and we will send you further instructions
-          </Typography>
-        </form>
-        <Button className={classNames.submit} type="submit">
-          Send Instructions
-        </Button>
-        <Typography className={classNames.question} variant={'body2'}>
-          {' '}
-          Did you remember your password?
-        </Typography>
-        <Typography as={'a'} href={''} className={classNames.signIn} variant={'h3'}>
-          {' '}
-          <Link to="/login">Try logging in</Link>
-        </Typography>
+          {editMode ? (
+            <>
+              <Typography className={classNames.info} variant={'h1'}>
+                {nameForPersonalInfo}
+                <EdittextIcon onClick={onEditTextClickHandler} />
+              </Typography>
+              <Typography className={classNames.question} variant={'body1'}>
+                {emailForPersonalInfo}
+              </Typography>
+              <Button className={classNames.submit} variant={'secondary'}>
+                <LogOut /> Logout
+              </Button>
+            </>
+          ) : (
+            <>
+              <CardsInput className={classNames.input} label={'Nickname'} />
+              <Button className={classNames.submit} variant={'primary'}>
+                Save Changes
+              </Button>
+            </>
+          )}
+        </div>
       </Card>
     </div>
   )
