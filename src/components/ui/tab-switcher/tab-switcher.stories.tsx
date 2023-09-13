a40ad7268e526461879f99da6ffda8a1e75e741f
krlkonstantine <krlkonstantine@gmail.com>
2023-09-13 07:19:08 +0300

    component elements implemented. Need to fix content and styles

---
+++ src/components/ui/tab-switcher/tab-switcher.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { TabSwitcher } from '@/components/ui/tab-switcher/tab-switcher.tsx'
+
+const meta = {
+  title: 'Components/Tab Switcher',
+  component: TabSwitcher,
+  tags: ['autodocs'],
+} satisfies Meta<typeof TabSwitcher>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    options: [
+      { label: 'My Cards', value: 'my-cards' },
+      { label: 'All Cards', value: 'all-cards' },
+    ],
+  },
+}
--- src/components/ui/tab-switcher/tab-switcher.tsx
+++ src/components/ui/tab-switcher/tab-switcher.tsx
@@ -1,8 +1,35 @@
 import { FC } from 'react'
 
+import { TextAlignLeftIcon, TextAlignRightIcon } from '@radix-ui/react-icons'
+import * as ToggleGroup from '@radix-ui/react-toggle-group'
+
+import s from './tab-switcher.module.scss'
+
+type ToggleOptions = {
+  label: string
+  value: string
+}
+
 type TabSwitcherProps = {
-  title: string
+  options: ToggleOptions[]
+  onValueChange?: () => void
+  className?: string
+  value?: any
 }
 export const TabSwitcher: FC<TabSwitcherProps> = () => {
-  return <div></div>
+  return (
+    <ToggleGroup.Root
+      className={s.ToggleGroup}
+      type="single"
+      defaultValue="center"
+      aria-label="Text alignment"
+    >
+      <ToggleGroup.Item className={s.ToggleGroupItem} value="left" aria-label="Left aligned">
+        <TextAlignLeftIcon />
+      </ToggleGroup.Item>
+      <ToggleGroup.Item className={s.ToggleGroupItem} value="right" aria-label="Right aligned">
+        <TextAlignRightIcon />
+      </ToggleGroup.Item>
+    </ToggleGroup.Root>
+  )
 }
