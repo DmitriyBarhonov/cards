7594b3cde134c75af90ed9fb7e856f1720681608
krlkonstantine <krlkonstantine@gmail.com>
2023-09-13 13:43:04 +0300

    feature: elements positioned in row, started to implement edit mode

--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -5,6 +5,8 @@ import clsx from 'clsx'
 
 import s from './slider.module.scss'
 
+import { Typography } from '@/components/ui'
+
 type ToggleOptionsType = {
   label: string
   value: string
@@ -14,35 +16,49 @@ type TabSwitcherProps = {
   options?: ToggleOptionsType[]
   disabled?: boolean
   defaultValue?: string
-  onValueChange?: (value: string) => void
+  maxCardsAmount?: number
+  onValueChange?: (min: number, max: number) => void
   className?: string
   value?: any
 }
 export const SliderForCards: FC<TabSwitcherProps> = ({ options, disabled, ...restProps }) => {
+  const [rangeValue, setRangeValue] = useState([0, 20])
+  const [editMode, setEditMode] = useState(false)
+
   const classNames = {
     toggleGroup: clsx(disabled ? s.toggleDisabled : s.toggleGroup),
     toggleGroupItem: clsx(disabled ? s.toggleDisabledItem : s.toggleGroupItem),
   }
-
-  const oValueChangeHandler = (value: string) => {
+  const activateEditMode = () => {
+    setEditMode(true)
+  }
+  const oValueChangeHandler = (value: number[]) => {
     //здесь будет отправляться запрос на показ
     //всех или только моих карточек
-    restProps.onValueChange && restProps.onValueChange(value)
+    setRangeValue(value)
+    restProps.onValueChange && restProps.onValueChange(value[0], value[1])
   }
 
   return (
-    <Slider.Root
-      className={s.SliderRoot}
-      defaultValue={[25, 75]}
-      max={100}
-      step={1}
-      minStepsBetweenThumbs={1}
-    >
-      <Slider.Track className={s.SliderTrack}>
-        <Slider.Range className={s.SliderRange} />
-      </Slider.Track>
-      <Slider.Thumb className={s.SliderThumb} aria-label="min-amount" />
-      <Slider.Thumb className={s.SliderThumb} aria-label="max-amount" />
-    </Slider.Root>
+    <div className={s.rangeContainer}>
+      <Typography onDoubleClick={activateEditMode} className={s.rangeDigit} variant={'body2'}>
+        {rangeValue[0]}
+      </Typography>
+      <Slider.Root
+        className={s.SliderRoot}
+        onValueChange={oValueChangeHandler}
+        defaultValue={rangeValue}
+        max={(restProps.maxCardsAmount && restProps.maxCardsAmount) || 100}
+        step={1}
+        minStepsBetweenThumbs={1}
+      >
+        <Slider.Track className={s.SliderTrack}>
+          <Slider.Range className={s.SliderRange} />
+        </Slider.Track>
+        <Slider.Thumb className={s.SliderThumb} aria-label="min-amount" />
+        <Slider.Thumb className={s.SliderThumb} aria-label="max-amount" />
+      </Slider.Root>
+      <Typography variant={'body2'}>{rangeValue[1]}</Typography>
+    </div>
   )
 }
