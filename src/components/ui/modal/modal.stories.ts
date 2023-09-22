16877caf62ff4b8d17a718e8566e3a27bbe71879
krlkonstantine <krlkonstantine@gmail.com>
2023-09-22 09:01:50 +0300

    feature: modal component added. Need to fix styles and logic

---
+++ src/components/ui/modal/modal.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Modal } from './index.ts'
+
+const meta = {
+  title: 'Components/Modal',
+  component: Modal,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Modal>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    modalTitle: 'Ah shit, here we go again',
+  },
+}
