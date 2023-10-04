80437790338c70d8d8cef2923606172ba6947973
Dmitriy <explay167@gmail.com>
2023-10-04 16:17:44 +0300

    fix logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import { Provider } from 'react-redux'
 
-import { Header } from './components/ui'
-
 import { Router } from '@/router.tsx'
 import { store } from '@/services/store.ts'
 
 export function App() {
   return (
     <Provider store={store}>
-      {/* <Router /> */}
-      <Header
-        isAuth={false}
-        onSignIn={function (): void {
-          throw new Error('Function not implemented.')
-        }}
-      />
+      <Router />
     </Provider>
   )
 }
