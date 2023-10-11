aecc672b8611afb7f2eca77a44430fada72115f3
Dmitriy <explay167@gmail.com>
2023-10-11 18:17:23 +0300

    small fix drop-down

--- src/components/ui/dropdown-menu/custom-drop-down/drop-down-user-info.tsx
+++ src/components/ui/dropdown-menu/custom-drop-down/drop-down-user-info.tsx
@@ -5,18 +5,18 @@ import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import s from './custom-drop-down.module.scss'
 
 type DropdownItemType = {
-  elment: ReactNode
+  element: ReactNode
   name: string
   email: string
   border: boolean
 }
-export const DropdownItemUserInfo: FC<DropdownItemType> = ({ elment, border, name, email }) => {
+export const DropdownItemUserInfo: FC<DropdownItemType> = ({ element, border, name, email }) => {
   const wrapper = border ? s.wrapperBorder : s.wrapper
 
   return (
     <DropdownMenu.Item className={s.DropdownMenuItemUser}>
       <div className={wrapper}>
-        <div className={s.element}>{elment}</div>
+        <div className={s.element}>{element}</div>
         <div className={s.userInfo}>
           <div className={s.name}>{name}</div>
           <div className={s.email}>{email}</div>
