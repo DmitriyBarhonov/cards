14a1f6731136c3161e65cb0fb5d06f5ac1122225
krlkonstantine <krlkonstantine@gmail.com>
2023-10-11 10:32:07 +0300

    dropdown implemented into header.Username is being displayed properly. Need to adjust dropdown styles

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,10 +1,14 @@
 import { FC } from 'react'
 
+import { Link } from 'react-router-dom'
+
 import { Logo } from '../../../assets/icons/Logo'
 
 import s from './header.module.scss'
 
-import { Button, Typography } from '@/components/ui'
+import { EdittextIcon } from '@/assets/icons/edit-text.tsx'
+import { PersonOutline } from '@/assets/icons/person-outline.tsx'
+import { Button, Dropdown, Typography } from '@/components/ui'
 import { Avatar } from '@/components/ui/avatar'
 
 type HeaderProps = {
@@ -33,7 +37,40 @@ export const Header: FC<HeaderProps> = ({
           <Typography variant="subtitle1" className={s.name}>
             {name || 'no name'}
           </Typography>
-          <Avatar name={name || 'no name'} avatar={avatar} />
+          <Dropdown
+            trigger={
+              <span>
+                <Avatar avatar={avatar} className={'m-17'} name={'Kilobuks Lover'} />
+              </span>
+            }
+            width={200}
+          >
+            <>
+              <div className={'flex'}>
+                <span className={'m-3'}>
+                  <Avatar name={'Kilobuks Lover'} />
+                </span>
+                <div className={'flex-col mt-2'}>
+                  <Typography variant={'h3'}>{'Kilobuks Lover'}</Typography>
+
+                  <span className={'text-zinc-400'}>
+                    <Typography variant={'body2'}>{'ilovekilubuks@gmail.com'}</Typography>
+                  </span>
+                </div>
+              </div>
+              <div className={'flex flex-col'}>
+                {/*    времннная дивка чтобы проверить ка котобразится иконка*/}
+                <Typography as={Link} to="/personal-info" variant={'h3'} className={'flex-row'}>
+                  <EdittextIcon style={{ color: 'green' }} /> {'My Profile'}
+                </Typography>
+                <Typography variant={'h3'}>
+                  {'Kilobuks Lover'}
+                  <PersonOutline className={'text-white'} width={20} height={20} />
+                </Typography>
+              </div>
+            </>
+          </Dropdown>
+          {/*<Avatar name={name || 'no name'} avatar={avatar} />*/}
         </div> //здесь будет закрывающий тег дропдауна
       ) : (
         <div className={s.button}>
