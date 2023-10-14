5f7e0e22a47f0cd6deba9553c53051c7be67edae
victoriapavlova <victoria.pavlova7@gmail.com>
2023-10-14 19:10:28 +0300

    fixed table classNames, implemented tabSwitchers for My/All decks, make adjustable size for PlayCircle icon, show icons at table weather the decks ar made by user or not

--- src/pages/decks-page/decks.tsx
+++ src/pages/decks-page/decks.tsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 
 import s from './decks.module.scss'
 
-import { Button, Input, Typography, Table, Pagination } from '@/components/ui'
+import { EdittextIcon } from '@/assets/icons/edit-text.tsx'
+import { PlayCircle } from '@/assets/icons/play-circle-outline.tsx'
+import { Button, Input, Typography, Table, Pagination, TabSwitcher } from '@/components/ui'
 import { SliderForCards } from '@/components/ui/slider'
 import { useAppDispatch, useAppSelector } from '@/hooks/hooks.ts'
+import { useGetMeQuery } from '@/services/auth'
 import { useCreateDeckMutation, useDeleteDeckMutation, useGetDecksQuery } from '@/services/decks'
 import { decksSlice } from '@/services/decks/decks.slice.ts'
 import { Deck } from '@/services/decks/decks.types.ts'
@@ -16,29 +19,45 @@ const columns: Column[] = [
   { key: 'created', title: 'Created by' },
   { key: 'action', title: 'Action' },
 ]
+const tabOptions = [
+  { label: 'My Cards', value: 'my' },
+  { label: 'All Cards', value: 'all' },
+]
 
 export const Decks = () => {
   const [sort, setSort] = useState<Sort>({ key: 'updated', direction: 'desc' })
   const sortString = sort ? `${sort.key}-${sort.direction}` : null //строка для бэкэнда
 
   // console.log(sort, sortString)
+  const [tabValue, setTabValue] = useState('my')
+
   const [search, setSearch] = useState('')
   const currentPage = useAppSelector(state => state.decks.currentPage)
   const dispatch = useAppDispatch()
   const updateCurrentPage = (page: number) => dispatch(decksSlice.actions.updateCurrentPage(page))
+
   const [itemsPerPage, setItemsPerPage] = useState(10)
+  const { data: user } = useGetMeQuery()
   const { currentData: decks } = useGetDecksQuery({
     currentPage,
     itemsPerPage,
     name: search,
     orderBy: sortString,
+    authorId: tabValue === 'my' ? user?.id : undefined,
+    //если на табе Моя колода, то в useGetDecksQuery передаст в параметр имя автора,
+    //то есть пользователя из useGetMeQuery
+    //если на табе будет Все колоды, то запрос пойдет с undefined, и покажутся все колоды
   })
   // totalPages: number
   // currentPage: number
   // itemsPerPage: number
   // totalItems: number
+
   const [deleteDeck] = useDeleteDeckMutation()
   const [createDeck, { isLoading }] = useCreateDeckMutation()
+  const tabHandler = (value: string) => {
+    setTabValue(value)
+  }
 
   //if (decks.isError) return <div>decks.isError</div>
   return (
@@ -53,7 +72,15 @@ export const Decks = () => {
           onChange={e => setSearch(e.currentTarget.value)}
           placeholder="Search by name"
         />
-        <SliderForCards disabled={false} />
+        <div>
+          <Typography variant={'caption'}>Show packs cards</Typography>
+          <TabSwitcher options={tabOptions} value={tabValue} onValueChange={tabHandler} />
+        </div>
+        <div>
+          <Typography variant={'caption'}>Number of cards</Typography>
+          <SliderForCards disabled={false} />
+        </div>
+
         <Button
           style={{ marginLeft: '6px' }}
           onClick={() => {
@@ -99,7 +126,11 @@ export const Decks = () => {
                 <Table.Data>{deck.cardsCount}</Table.Data>
                 <Table.Data>{new Date(deck.updated).toLocaleDateString('ru-Ru')}</Table.Data>
                 <Table.Data>{deck.author.name}</Table.Data>
-                <Table.Data>
+                <Table.Data className={s.iconsRow}>
+                  <PlayCircle size={24} />
+                  {/*если это моя колода, то покажи все иконки, иначе только learn */}
+                  {user.id === deck.author.id ? <EdittextIcon /> : null}
+
                   <Button
                     onClick={() => {
                       deleteDeck({ id: deck.id })
