4113f5abc0ae23146343401c9213b7bfb80d272c
krlkonstantine <krlkonstantine@gmail.com>
2023-10-24 12:07:07 +0300

    added useEffect to useDebounce

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 
 import { useAppSelector } from '@/hooks/hooks'
+import { useGetDecksQuery } from '@/services/decks'
 
 function useDebounceFunction<T>(value: T, delay?: number): T {
   const [state, setState] = useState<T>(value)
@@ -17,11 +18,17 @@ function useDebounceFunction<T>(value: T, delay?: number): T {
 }
 
 export const useDebounce = () => {
+  const { currentData: decks } = useGetDecksQuery()
   const { maxCardsCount, minCardsCount } = useAppSelector(state => state.decks.cardsCount)
 
   const [cardsCount, setCardsCount] = useState<number[]>([minCardsCount, maxCardsCount])
 
   const cardsCountSettings = useDebounceFunction(cardsCount)
+
+  useEffect(() => {
+    debugger
+    setCardsCount([cardsCount[0], decks?.maxCardsCount ? decks?.maxCardsCount : cardsCount[1]])
+  }, [])
 
   const resetSlider = () => {
     setCardsCount([minCardsCount, maxCardsCount])
