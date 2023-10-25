7197ec6fae155bd439e81544c01be3bd8876f514
krlkonstantine <krlkonstantine@gmail.com>
2023-10-25 13:31:02 +0300

    input added. Drag and drop implemented. Need to improve input

--- src/components/decks/add-upgrade-deck/add-upgrade-deck.tsx
+++ src/components/decks/add-upgrade-deck/add-upgrade-deck.tsx
@@ -1,10 +1,12 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
-import { Button, Modal } from '@/components/ui'
+import s from './add-upgrade-deck.module.scss'
+
+import { Button, Modal, Typography } from '@/components/ui'
 import { ControlledCheckbox } from '@/components/ui/controlled'
 import { ControlledInput } from '@/components/ui/controlled-input'
 
@@ -45,6 +47,9 @@ export const AddUpgradeDeck: FC<AddUpgradeDeckProps> = ({
     defaultValues, //берем из пропсов, по умолчанию пустые
   })
   ///
+
+  const [drag, setDrag] = useState<boolean>(false)
+
   const onSubmit = (data: AddUpgradeType) => {
     deckHandler(data)
     resetField('name')
@@ -59,6 +64,14 @@ export const AddUpgradeDeck: FC<AddUpgradeDeckProps> = ({
     reset()
     toggleModal(false)
   }
+  const dragStartHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setDrag(true)
+  }
+  const dragLeaveHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setDrag(false)
+  }
 
   return (
     <Modal
@@ -68,23 +81,40 @@ export const AddUpgradeDeck: FC<AddUpgradeDeckProps> = ({
       modalMainTitle={title}
     >
       <form onSubmit={handleFormSubmitted}>
-        <ControlledInput
-          name="name"
-          variant={'standard'}
-          placeholder={defaultValues.name}
-          label={'New pack name'}
-          control={control}
-          autoComplete="false"
-        />
-        <div className={'my-8'}>
-          <ControlledCheckbox name="isPrivate" label={'Private pack'} control={control} />
-        </div>
-
-        <div className={'flex justify-between'}>
-          <Button type="button" onClick={onCloseHandler} variant={'secondary'}>
-            Cancel
-          </Button>
-          <Button type="submit">{buttonText}</Button>
+        <div className={s.dragAndDropArea}>
+          {drag ? (
+            <div
+              className={s.dropArea}
+              onDragLeave={e => dragLeaveHandler(e)}
+              onDragOver={e => dragStartHandler(e)}
+            >
+              <Typography variant={'h2'}>Release the new file here</Typography>
+            </div>
+          ) : (
+            <div onDragLeave={e => dragLeaveHandler(e)} onDragOver={e => dragStartHandler(e)}>
+              Drag file here for upload, or
+              <div>
+                <input type="file" />
+              </div>
+              <ControlledInput
+                name="name"
+                variant={'standard'}
+                placeholder={defaultValues.name}
+                label={'New pack name'}
+                control={control}
+                autoComplete="false"
+              />
+              <div className={'my-8'}>
+                <ControlledCheckbox name="isPrivate" label={'Private pack'} control={control} />
+              </div>
+              <div className={'flex justify-between'}>
+                <Button type="button" onClick={onCloseHandler} variant={'secondary'}>
+                  Cancel
+                </Button>
+                <Button type="submit">{buttonText}</Button>
+              </div>
+            </div>
+          )}
         </div>
       </form>
     </Modal>
