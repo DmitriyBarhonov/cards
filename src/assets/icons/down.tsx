4a31853e39e7ac6ed82800396a5a9d5505b1d508
victoriapavlova <victoria.pavlova7@gmail.com>
2023-10-26 12:41:15 +0300

    added sort for cards, fixed table arrow

--- src/assets/icons/down.tsx
+++ src/assets/icons/down.tsx
@@ -3,7 +3,7 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) =
   <svg
     width="24"
     height="15"
-    viewBox="0 0 24 15"
+    viewBox="0 4 24 15"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     ref={ref}
--- src/services/decks/decks.types.ts
+++ src/services/decks/decks.types.ts
@@ -53,7 +53,7 @@ export type GetCardsDeckParams = {
   id: string
   question?: string
   answer?: string
-  orderBy?: string
+  orderBy?: string | null
   currentPage?: number
   itemsPerPage?: number
 }
