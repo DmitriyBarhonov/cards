c9a2d730f29003f5cd59f2085be05f336072542c
krlkonstantine <krlkonstantine@gmail.com>
2023-10-31 13:18:44 +0300

    types for card service moved from deckTypes to cardTypes

--- src/services/cards/cards.types.ts
+++ src/services/cards/cards.types.ts
@@ -9,3 +9,61 @@ export type UpdateCardParams = FormData
 //   questionVideo?: string
 //   answerVideo?: string
 // }
+
+// Cards type
+export type Card = {
+  id: string
+  question: string
+  answer: string
+  deckId: string
+  questionImg: string | null
+  answerImg: string | null
+  questionVideo: string | null
+  answerVideo: string | null
+  created: string
+  updated: string
+  shots: number
+  grade: number
+  userId: string
+}
+
+export type CardsResponse = {
+  pagination: Pagination
+  items: Card[]
+}
+
+export type InitialStateType = {
+  currentPage: number
+  itemsPerPage: string
+  tabValue: string
+  cardsCount: {
+    minCardsCount: number
+    maxCardsCount: number
+  }
+}
+export type CreateCardParams = {
+  question: string
+  answer: string
+  questionImg?: string
+  answerImg?: string
+  questionVideo?: string
+  answerVideo?: string
+}
+
+export type GetRandomCard = {
+  id: string
+  previousCardId?: string
+}
+export type SaveTheGrade = {
+  cardId: string
+  grade: number
+}
+
+export type GetCardsDeckParams = {
+  id: string
+  question?: string
+  answer?: string
+  orderBy?: string | null
+  currentPage?: number
+  itemsPerPage?: number
+}
