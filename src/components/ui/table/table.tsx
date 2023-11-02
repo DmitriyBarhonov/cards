bfbe1a1b648dd80995ff41de0c3b5b8959c28f9b
krlkonstantine <krlkonstantine@gmail.com>
2023-11-02 09:13:39 +0300

    small change

--- src/components/ui/table/table.tsx
+++ src/components/ui/table/table.tsx
@@ -9,7 +9,8 @@ import { SortUp } from '@/assets/icons/up.tsx'
 import { Typography } from '@/components/ui'
 import { Column, Sort } from '@/services/types'
 
-//везде будет одинаковая конструкция ComponentProps<'тег таблицы'>, то есть кастомный компонент будет принимать пропсы такие же, как и тег таблицы
+//везде будет одинаковая конструкция ComponentProps<'тег таблицы'>,
+// то есть кастомный компонент будет принимать пропсы такие же, как и тег таблицы
 //будут представлены все компоненты table, thead, tbody, th, tr, td
 export const Root: FC<ComponentProps<'table'>> = ({ className, ...rest }) => {
   const classNames = {
